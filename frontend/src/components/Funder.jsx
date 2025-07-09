b5855ac74c8d873d62ec6c36d76f376613decc15
Uttkarsh Pawan <utkbest@gmail.com>
2025-07-10 02:19:09 +0530

    complete contract integration

--- frontend/src/components/Funder.jsx
+++ frontend/src/components/Funder.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { ethers } from 'ethers';
+import { getContract } from '../utils/contract.js';
 
 const Funder = () => {
+    const [amount, setAmount] = useState("");
+    const [balance, setBalance] = useState("0");
+
+    const fundContract = async () => {
+        const contract = await getContract();
+        const tx = await contract.sendETHtoContract({
+            value : ethers.parseEther(amount)
+        });
+        await tx.wait();
+        alert("Deposited");
+    }
+
+    const getBalance = async () => {
+        const contract = await getContract();
+        const bal = await contract.getContractBalance();
+        setBalance(ethers.formatEther(bal));
+    }
+
+    const withdrawEth = async () => {
+        const contract = await getContract();
+        const tx = await contract.withdraw();
+        await tx.wait();
+    }
+
   return (
     <div className="bg-teal-400 h-screen flex flex-col justify-center items-center">
         <div className="border-2 border-gray-700 rounded flex-col justify-center items-center mb-5">
             Fund Contract
             <div className="flex">
-            <input className="h-8 m-2" type='number' ></input>
-            <button className="bg-emerald-600 text-black px-4 py-2 rounded h-8 w-32 items-center flex m-2"> Fund </button></div>
+            <input className="h-8 m-2" type="text" value={amount} onChange={(e) => setAmount(e.target.value)}></input>
+            <button className="bg-emerald-600 text-black px-4 py-2 rounded h-8 w-32 items-center flex m-2" onClick={fundContract}> Fund </button></div>
         </div>
         <div className="border-2 border-gray-700 rounded flex-col justify-center items-center mt-5 w-64">
-            Get Contract Balance<br/> amount
+            Get Contract Balance<br/> {balance}
             <div className="flex">
-            <button className="bg-emerald-600 text-black px-4 py-2 rounded h-8 w-32 items-center flex m-2"> Balance </button>
-            <button className="bg-emerald-600 text-black px-4 py-2 rounded h-8 w-32 items-center flex m-2"> Withdraw </button></div>
+            <button className="bg-emerald-600 text-black px-4 py-2 rounded h-8 w-32 items-center flex m-2" onClick={getBalance}> Balance </button>
+            <button className="bg-emerald-600 text-black px-4 py-2 rounded h-8 w-32 items-center flex m-2" onClick={withdrawEth}> Withdraw </button></div>
         </div>
     </div>
 
--- frontend/src/components/WalletInteract.jsx
+++ frontend/src/components/WalletInteract.jsx
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react';
+import { ethers } from 'ethers';
+import { getContract } from '../utils/contract.js';
 
 const WalletInteract = () => {
-  return (
-    <div className="bg-teal-400 h-screen flex flex-col justify-center items-center">
-        <div className="border-2 border-gray-700 rounded flex-col justify-center items-center mb-5">
-            Send Account Ether
-            <div className="flex">
-            <input className="h-8 m-2" type='number' ></input>
-            <button className="bg-emerald-600 text-black px-4 py-2 rounded h-8 w-32 items-center flex m-2"> Fund </button></div>
+    const [amount, setAmount] = useState("");
+    const [address, setAddress] = useState("");
+    const [balance, setBalance] = useState("0");
+
+    const sendETH = async () => {
+        const contract = await getContract();
+        const tx = await contract.sendETHtoUser(address, {
+            value: ethers.parseEther(amount)
+        });
+        await tx.wait();
+        alert(`Sent ${amount} ETH to ${address}`)
+    }
+
+    const getBal = async () => {
+        const contract = await getContract();
+        const bal = await contract.getUserBalance(address);
+        setBalance(ethers.formatEther(bal));
+    }
+
+    return (
+        <div className="bg-teal-400 h-screen flex flex-col justify-center items-center">
+            <div className="border-2 border-gray-700 rounded flex-col justify-center items-center mb-5">
+                Send Account Ether
+                <div className="flex">
+                <input className="h-8 m-2" type="text" placeholder="Address" value={address} onChange={(e)=> setAddress(e.target.value)}></input>
+                <input className="h-8 m-2" type="text" placeholder="Amount" value={amount} onChange={(e)=>setAmount(e.target.value)}></input>
+                <button className="bg-emerald-600 text-black px-4 py-2 rounded h-8 w-32 items-center flex m-2" onClick={sendETH}> Fund </button></div>
+            </div>
+            <div className="border-2 border-gray-700 rounded flex-col justify-center items-center w-64 mt-5">
+                Get Account Balance <br/>
+                {balance}
+                <div className="flex">
+                <button className="bg-emerald-600 text-black px-4 py-2 rounded h-8 w-32 items-center flex m-2" onClick={getBal}> Balance </button>
+            </div>
+            
         </div>
-        <div className="border-2 border-gray-700 rounded flex-col justify-center items-center w-64 mt-5">
-            Get Account Balance <br/>
-            amount
-            <div className="flex">
-            <button className="bg-emerald-600 text-black px-4 py-2 rounded h-8 w-32 items-center flex m-2"> Balance </button>
         </div>
-        
-    </div>
-    </div>
-  )
+    )
 }
 
 export default WalletInteract
