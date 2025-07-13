1f4b389897fecd78ef6a851431dc280d8319cd77
Uttkarsh Pawan <utkbest@gmail.com>
2025-07-13 20:32:14 +0530

    Add Homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 const Home = () => {
   return (
-    <div>Home</div>
+     <div className="bg-teal-400 h-screen flex flex-col justify-center items-center">
+        <p className="text-2xl font-mono"> Welcome to </p><br/>
+        <p className="text-6xl font-serif"> ETH Funder </p><br/><br/>
+        <Link to="/funder" className='font-mono'>Fund Me</Link>
+        <Link to="/walletinteract" className='font-mono'>Fund Someone Else</Link>
+    </div>
   )
 }
 
